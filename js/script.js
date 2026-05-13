844c759fad3496774593ce16f6d0bc24aedef33e
Rami <92794924+Ramikuteish@users.noreply.github.com>
2026-05-13 15:56:10 +0200

    Add files via upload
    Circle Follow The Cursor

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 /* jshint esversion: 6 */
 
+// Circle Follow the Cursor
+const circleElement = document.querySelector('.circle');
+
+const mouse = { x: 0, y: 0 };
+const circle = { x: 0, y: 0 };
+
+window.addEventListener('mousemove', e => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+const speed = 0.15;
+
+const tick = () => {
+    circle.x += (mouse.x - circle.x) * speed;
+    circle.y += (mouse.y - circle.y) * speed;
+
+    circleElement.style.transform = `translate(${circle.x}px, ${circle.y}px)`;
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
+
+
 // Zurück-Button Funktion
 function backFunction() {
     window.location.href = "../index.html";
